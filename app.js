fd4994218ece11de94b40445b3a7b097818ad7d9
Adrian Simental <asimental100@hotmail.com>
2020-06-24 16:10:32 -0700

    almost there

---
+++ app.js
@@ -0,0 +1,20 @@
+const changeToPinkButton = document.getElementById("pink-button");
+
+const changeToLightGreenButton = document.getElementById("light-green-button");
+
+const changeToLightBlueButton = document.getElementById("light-blue-button");
+
+changeToPinkButton.addEventListener('click', () => {
+    bigboi.style.backgroundColor = "Pink";
+    bigboi.style.border = "solid pink 10px";
+})
+
+changeToLightGreenButton.addEventListener('click', () => {
+    bigboi.style.backgroundColor = "lightGreen";
+    bigboi.style.border = "solid lightGreen 10px";
+})
+
+changeToLightBlueButton.addEventListener('click', () => {
+    bigboi.style.backgroundColor = "lightBlue";
+    bigboi.style.border = "solid lightBlue 10px";
+})
